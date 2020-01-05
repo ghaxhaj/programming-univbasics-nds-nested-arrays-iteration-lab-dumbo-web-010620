db2719debf0b97607701068b455d8b6614d3ae83
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-05 23:57:04 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -49,11 +49,10 @@ def total_even_pairs(src)
   total = 0
   row_index = 0
   while row_index < src.length do
-    (src[row_index][0] % 2 == 0) && (src[row_index][1] % 2 == 0)
+    if (src[row_index][0] % 2 == 0) && (src[row_index][1] % 2 == 0)
     total += (src[row_index][0] + src[row_index][1])
   end
     row_index +=1
   end
   total
-
 end
